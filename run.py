ecad880cd59d96c3b253ef0882f125a770b38704
impshum <impshum@impshum.co.uk>
2020-02-28 13:18:15 +0000

    Update run.py

--- run.py
+++ run.py
@@ -155,7 +155,6 @@ def nbo_scraper(first_run=False):
         bno_db.dump()
     print(f'old: {old} new: {new}')
 
-
 def lovely_soup(u):
     r = get(u, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'})
     return BeautifulSoup(r.text, 'lxml')
